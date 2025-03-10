3460690ce4e552881f07a298b95fba3791d8de3e
Guilherme <guilhermevbagio@gmail.com>
2025-03-10 14:48:52 -0300

    aula completa

--- app/src/main/java/cursotdd/tradutor/Tradutor.java
+++ app/src/main/java/cursotdd/tradutor/Tradutor.java
@@ -1,7 +1,40 @@
 package cursotdd.tradutor;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Tradutor {
+    private Map<String, String> traducoes = new HashMap<>();
+
     public boolean estaVazio() {
-        return true;
+        return traducoes.isEmpty();
     }
+
+    public void adicionaTraducao(String palavra, String traducao){
+        if(traducoes.containsKey(palavra)){
+            traducao = traduzir(palavra) + ", " + traducao;
+        }
+        traducoes.put(palavra, traducao);
+    }
+
+    public String traduzir(String arg){
+        return traducoes.get(arg);
+    }
+    private String primeiraTraducao(String palavra){
+        String traducao = traduzir(palavra);
+        if(traducao.contains(","))
+            return traducao.substring(0, traducao.indexOf(",")) + " ";
+        else
+            return traducao + " ";
+    }
+    public String traduzirFrase(String frase){
+        String[] palavras = frase.split(" ");
+        String fraseTraduzida = "";
+        for(String palavra : palavras){
+            fraseTraduzida += primeiraTraducao(palavra);
+        }
+        return fraseTraduzida.trim();
+    }
+    
+
+
 }
--- app/src/test/java/cursotdd/tradutor/TesteTradutor.java
+++ app/src/test/java/cursotdd/tradutor/TesteTradutor.java
@@ -2,11 +2,55 @@ package cursotdd.tradutor;
 
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*; 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach; 
 
 public class TesteTradutor {
+
+    private Tradutor t;
+
+    @BeforeEach
+    public void criarTradutor(){
+        t = new Tradutor();
+    }
+
     @Test public void tradutorSemPalavras(){
-        Tradutor t = new Tradutor();
         assertTrue(t.estaVazio());
+    }
+
+    @Test public void umaTraducao(){
+        t.adicionaTraducao("um", "one");
+        assertEquals("one", t.traduzir("um"));
+    }
+
+    @Test public void duasTraducoes(){
+        t.adicionaTraducao("um", "one");
+        t.adicionaTraducao("dois", "two");
+        assertEquals("one", t.traduzir("um"));
+        assertEquals("two", t.traduzir("dois"));
+    }
+
+    @Test public void duasTraducoesMesmaPalavra(){
+        t.adicionaTraducao("bom", "good");
+        t.adicionaTraducao("bom", "nice");
+        assertEquals("good, nice", t.traduzir("bom"));
+    }
+    @Test public void traduzirFrase(){
+        t.adicionaTraducao("guerra", "war");
+        t.adicionaTraducao("é", "is");
+        t.adicionaTraducao("ruim", "bad");
+
+        assertEquals("war is bad", t.traduzirFrase("guerra é ruim"));
+    }
+
+    @Test public void traduzirFraseComMultiplasTraducoes(){
+        t.adicionaTraducao("guerra", "war");
+        t.adicionaTraducao("é", "is");
+        t.adicionaTraducao("ruim", "bad");
+        t.adicionaTraducao("ruim", "awful");
+
+
+        assertEquals("war is bad", t.traduzirFrase("guerra é ruim"));
     }
 }
